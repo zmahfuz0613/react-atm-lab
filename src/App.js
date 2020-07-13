bd9c86d158eecfe999371f46431e7f301ef4a0c3
aspittel <alison.spittel@generalassemb.ly>
2020-07-13 14:07:49 -0500

    hooks

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React, { Component } from 'react';
 import logo from './ga.png';
 import Account from './Account';
 
-class App extends Component {
-  render() {
-    return (
-      <div id="content">
-        <div id="nav">
-          <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
-          <div id="title">Bank of GA</div>
-        </div>
-        <Account />
-        <Account />
-        <div className="clear"></div>
+function App() {
+  return (
+    <div id="content">
+      <div id="nav">
+        <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
+        <div id="title">Bank of GA</div>
       </div>
-    );
-  }
+      <Account />
+      <Account />
+      <div className="clear"></div>
+    </div>
+  );
 }
 
 export default App;
