b4d639a8dd6d55d744cf99eb19ce506b2af07d48
zmahfuz061318 <str053mgr@gmail.com>
2020-07-21 20:05:02 -0400

    work on progress

--- src/Account/index.js
+++ src/Account/index.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, {useState} from 'react'
 
-
-function Account() {
+function Account(props) {
+ 
+//     const handleSubmit = e => {
+  
+//       e.preventDefault()
+    
+//       const balanceCopy = [...balance]
+//       const newBalance = {
+//         balance: balance
+//       }
+//       balanceCopy.push(newBalance)
+//       updateBalance(balanceCopy)
+//     }
+// }
   return (
     <div className="account">
-      <h2>TODO: CHANGE TO PROP</h2>
+      <h2>{props.name}</h2>
       <div className="balance">$0</div>
       <input type="text" placeholder="enter an amount" />
       <input type="button" value="Deposit" />
--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import logo from './ga.png';
 import Account from './Account';
 
 function App() {
+
   return (
     <div id="content">
       <div id="nav">
         <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
         <div id="title">Bank of GA</div>
       </div>
-      <Account />
-      <Account />
+
+      <Account name="Checking" />
+      <Account name="Savings"/>
       <div className="clear"></div>
     </div>
   );
